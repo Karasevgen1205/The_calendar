5e1dbcc6a93f2c4dcb0d494fb4b804592606da06
Yauheni <Karasevgen1205@yandex.ru>
2021-01-25 20:54:21 +0300

    The calendar add models

--- app_calendar/models.py
+++ app_calendar/models.py
@@ -1,3 +1,25 @@
+import calendar
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Holiday(models.Model):
+    title = models.CharField(max_length=50)
+    date = models.DateField()
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, null=True, blank=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class User(AbstractUser):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, null=True, blank=True)
+
