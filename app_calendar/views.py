bb10fd8cce8be7f0af21934d63ad858ddfb3b72a
Yauheni <Karasevgen1205@yandex.ru>
2021-01-26 22:05:38 +0300

    The calendar add country_filter

--- app_calendar/views.py
+++ app_calendar/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django_filters.rest_framework import FilterSet, RangeFilter, BaseInFilter, CharFilter
 
-# Create your views here.
+from app_calendar.models import Country
+
+
+class CharFilterInFilter(BaseInFilter, CharFilter):
+    pass
+
+class CountryFilter(FilterSet):
+    name = CharFilterInFilter(field_name='country__name', lookup_expr='in')
+    id = RangeFilter()
+
+    class Meta:
+        model = Country
+        fields = ['id', 'name']
