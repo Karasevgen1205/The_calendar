0b0494f0cc275a5a5d735f8d203fa022b98062be
Yauheni <Karasevgen1205@yandex.ru>
2021-01-30 20:48:54 +0300

    The calendar add country_filter 4

--- the_calendar/settings.py
+++ the_calendar/settings.py
@@ -123,3 +123,4 @@ STATIC_URL = '/static/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = "karasevgen1205@yandex.ru"
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
